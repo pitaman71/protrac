4b81034a10a1ae1b7055f5c80427794ee703e46c
Alan Pita <pitaman@mac.com>
2016-09-22 18:50:41 -0500

    Add separate specs for <labelItem> and <labelList> Finish forms to permit user to add entries

--- generators/generate_jdx_client.py
+++ generators/generate_jdx_client.py
@@ -11,8 +11,22 @@ class NodeWrapper:
 
   # returns a 1-2 word description of this item
   # used for tab labels, button labels, etc.
-  def getLabel(self):
-    nodes = self.defNode.findall('label')
+  def getLabelItem(self):
+    nodes = self.defNode.findall('labelItem')
+    if(len(nodes) == 0):
+      return self.defNode.get('name')
+    else:
+      node = nodes[len(nodes)-1]
+      result = ""
+      for child in node.iter():
+        if child != node:
+          result += ElementTree.tostring(child)
+      return result % node.attrib
+
+  # returns a 1-2 word description of (lists of) this kind of item
+  # used for tab labels, button labels, etc.
+  def getLabelList(self):
+    nodes = self.defNode.findall('labelList')
     if(len(nodes) == 0):
       return self.defNode.get('name')
     else:
@@ -37,13 +51,33 @@ class NodeWrapper:
       node = nodes[len(nodes)-1]
       return node.text % node.attrib
 
+class PropertyWrapper(NodeWrapper):
+  def __init__(self,rootNode,defNode):
+    NodeWrapper.__init__(self,rootNode,defNode)
+    self.typeNode = xref(rootNode,defNode.get('type'),False)
+
+  def emitFormElement(self):
+      if(self.typeNode is not None):
+        print """
+        <Dropdown
+          auto
+          onChange={this.handle%(name)sChange}
+          source={this.props.%(name)sValues}
+          value={this.state.editorValues.%(name)s}
+          label="%(label)s"
+        />    """ % dict(name=self.defNode.get('name'),label=NodeWrapper(root,self.defNode).getLabelList())
+      elif(self.defNode.get('type') == 'StringLabel'):
+        print """<Input type="text" multiline={false} onChange={this.handle%(name)sChange} label="%(label)s" value={this.state.editorValues.%(name)s}/>""" % dict(name=self.defNode.get('name'),label=self.getLabelList())
+      elif(self.defNode.get('type') == 'StringComment'):
+        print """<Input type="text" multiline={true} onChange={this.handle%(name)sChange} label="%(label)s" value={this.state.editorValues.%(name)s}/>""" % dict(name=self.defNode.get('name'),label=self.getLabelList())
+
 class BrowseWrapper(NodeWrapper):
   def __init__(self,rootNode,defNode):
     NodeWrapper.__init__(self,rootNode,defNode)
-    self.typeNode = xref(rootNode,defNode.get('type'))
+    self.typeNode = xref(rootNode,defNode.get('type'),True)
 
-  def getLabel(self):
-    labelNodes = self.typeNode.findall('label')
+  def getLabelList(self):
+    labelNodes = self.typeNode.findall('labelList')
     if(len(labelNodes) == 0):
       return self.defNode.get('type')
     else:
@@ -53,7 +87,7 @@ class BrowseWrapper(NodeWrapper):
   def getListSummary(self):
     nodes = self.typeNode.findall('listSummary')
     if(len(nodes) == 0):
-      return self.getLabel()
+      return self.getLabelList()
     else:
       node = nodes[len(nodes)-1]
       return node.text % node.attrib
@@ -63,28 +97,31 @@ class BrowseWrapper(NodeWrapper):
           <Tab label="%(label)s">
             <%(type)sBrowser url="/api/%(type)s" pollInterval={2000} />
           </Tab>
-          """ % dict(label=self.getLabel(),type=self.defNode.get('type'))
+          """ % dict(label=self.getLabelList(),type=self.defNode.get('type'))
 
-def xref(rootNode,name):
+def xref(rootNode,name,mandatory):
   predicate = 'objType[@name=%s]' % (name)
-  sys.stderr.write("predicate is %s\n" % predicate)
   defs1 = rootNode.findall("objType[@name='%s']" % (name))
   defs2 = rootNode.findall("relType[@name='%s']" % (name))
   if(len(defs1) > 0):
     return defs1[len(defs1)-1]
   elif(len(defs2) > 0):
     return defs2[len(defs2)-1]
+  elif mandatory:
+    raise Exception("Cannot locate definition of %s\n" % name)
   else:
-    sys.stderr.write("Cannot locate definition of %s\n" % name)
-    exit(1)
+    return None
 
 print """
 const React = require('react');
 const ReactDOM = require('react-dom');
 const toolbox = require('react-toolbox');
+const Input = toolbox.Input;
 const Tabs = toolbox.Tabs;
 const Tab = toolbox.Tab;
 const Dropdown = toolbox.Dropdown;
+const Button = toolbox.Button;
+const Dialog = toolbox.Dialog;
 """
 
 argi = 0
@@ -163,13 +200,42 @@ class %(name)s extends React.Component {
     for objType in root.findall('objType'):
         print """
 var %(name)s = React.createClass({
+        """ % objType.attrib
+        for propertyType in objType.findall('propertyType'):
+            propTypeName = ""
+            if 'type' in propertyType.attrib:
+                propTypeName = propertyType.get('type')
+            userDefs = root.findall('objType[@name=\'%s\']' % propTypeName)
+            if len(userDefs) > 0:
+                userDef = userDefs[len(userDefs)-1]
+                print """
+  getLabel_%(name)s: function() {
+    var selectedValue = this.props.%(name)sValues.find(function(item) { return item.value == this.state.%(name)s; }.bind(this));
+    if(selectedValue) {
+      return selectedValue.label;
+    } else {
+      return '';
+    }
+  },                  """ % propertyType.attrib
+        print """
+  getInitialState: function() {
+      return {"""
+        count = 0
+        for propertyType in objType.findall('propertyType'):
+          if count != 0:
+            print """, """
+          count += 1
+          print """%(name)s: this.props.%(name)s""" % propertyType.attrib
+        print """
+      };
+  },
   render: function() {
         """ % objType.attrib
 
         for propertyType in objType.findall('propertyType'):
             propTypeName = ""
-#            if 'type' in propertyType.attrib:
-#                propTypeName = propertyType.get('type')
+            if 'type' in propertyType.attrib:
+                propTypeName = propertyType.get('type')
             userDefs = root.findall('objType[@name=\'%s\']' % propTypeName)
             if len(userDefs) > 0:
                 print """
@@ -216,11 +282,11 @@ var %(name)sList = React.createClass({
 
         for propertyType in objType.findall('propertyType'):
             propTypeName = ""
-#            if 'type' in propertyType.attrib:
-#                propTypeName = propertyType.get('type')
+            if 'type' in propertyType.attrib:
+                propTypeName = propertyType.get('type')
             userDefs = root.findall('objType[@name=\'%s\']' % propTypeName)
             if len(userDefs) > 0:
-                print """ %(name)sID={row.%(name)sID}""" % propertyType.attrib
+                print """ %(name)s={row.%(name)s} %(name)sValues={this.props.%(name)sValues}""" % propertyType.attrib
             else:
                 print """ %(name)s={row.%(name)s}""" % propertyType.attrib
 
@@ -256,20 +322,16 @@ var %(name)sBrowser = React.createClass({
                 userDef = userDefs[len(userDefs)-1]
                 print """
   %(name)sLoaded: function(rows) {
-    var values = [];
+    var items = [];
     for(index=0;index<rows.length;index++) {
-      var value = new %(name)s;
-      value.setState({""" % propertyType.attrib
-                index = 0
+      var item = { value: rows[index].id };
+      item.label = '%(label)s'; """ % dict(name=propertyType.get('name'),label=NodeWrapper(root,userDef).getLabelItem())
                 for subProperty in userDef.findall('propertyType'):
-                    if(index != 0): 
-                      print ""","""
-                    index += 1
-                    print """ %(name)s: rows[index].%(name)s""" % subProperty.attrib
-                print """});
-      values.push(value);
+                    print """    item.label = item.label.replace(/<%(name)s\s*\/>/g,rows[index].%(name)s);\n""" % subProperty.attrib
+                print """
+      items.push(item);
     }
-    this.setState({%(name)sValues: values});
+    this.setState({%(name)sValues: items});
   },
                 """ % propertyType.attrib
         print """
@@ -314,12 +376,12 @@ var %(name)sBrowser = React.createClass({
                 """ % propertyType.attrib
         print """
   },
-  handleSubmit: function(row) {
+  handleSubmit: function(editorState) {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
       type: 'POST',
-      data: row,
+      data: editorState,
       success: function(data) {
         this.setState({data: data});
       }.bind(this),
@@ -328,8 +390,9 @@ var %(name)sBrowser = React.createClass({
       }.bind(this)
     });
   },
+
   getInitialState: function() {
-    return {data: []""" % objType.attrib
+    return {data: []""" % dict(name=objType.get('name'),label=NodeWrapper(root,objType).getLabelList())
 
         for propertyType in objType.findall('propertyType'):
             propTypeName = ""
@@ -356,7 +419,17 @@ var %(name)sBrowser = React.createClass({
             userDefs = root.findall('objType[@name=\'%s\']' % propTypeName)
             if len(userDefs) > 0:
                 userDef = userDefs[len(userDefs)-1]
-                print """ %(name)sValues={this.state.studentValues}""" % propertyType.attrib
+                print """ %(name)sValues={this.state.%(name)sValues}""" % propertyType.attrib
+        print """/>
+          <%(name)sForm onSubmit={this.handleSubmit} """ % dict(name=objType.get('name'),label=NodeWrapper(root,objType).getLabelList())
+        for propertyType in objType.findall('propertyType'):
+            propTypeName = ""
+            if 'type' in propertyType.attrib:
+                propTypeName = propertyType.get('type')
+            userDefs = root.findall('objType[@name=\'%s\']' % propTypeName)
+            if len(userDefs) > 0:
+                userDef = userDefs[len(userDefs)-1]
+                print """ %(name)sValues={this.state.%(name)sValues}""" % propertyType.attrib
         print """/>
       </div>
     );
@@ -364,6 +437,76 @@ var %(name)sBrowser = React.createClass({
 });
 
         """ % objType.attrib
+
+        print """
+var %(name)sForm = React.createClass({
+        """ % objType.attrib
+
+        for propertyType in objType.findall('propertyType'):
+            propTypeName = ""
+            if 'type' in propertyType.attrib:
+                propTypeName = propertyType.get('type')
+            userDefs = root.findall('objType[@name=\'%s\']' % propTypeName)
+            if len(userDefs) > 0:
+                userDef = userDefs[len(userDefs)-1]
+                print """        %(name)sValues: [],""" % propertyType.attrib
+        print """
+  getInitialState: function() {
+    return { actions: [
+    { label: "Cancel", onClick: this.handleToggle },
+    { label: "Add %(label)s", onClick: this.handleSubmit }
+                      ], 
+             editorActive: false, editorValues: {""" % dict(name=objType.get('name'),label=NodeWrapper(root,objType).getLabelList())
+        propCount = 0
+        for propertyType in objType.findall('propertyType'):
+            if(propCount != 0):
+              print """, """
+            print """%(name)s: ''""" % propertyType.attrib
+            propCount += 1
+
+        print """}};
+  },
+  handleToggle: function() {
+    this.setState({editorActive: !this.state.editorActive});
+  },
+  handleSubmit: function() {
+    this.props.onSubmit(this.state.editorValues);
+    this.setState({editorActive: false});
+  },
+              """
+        for propertyType in objType.findall('propertyType'):
+            print """      
+  handle%(name)sChange: function(value) {
+    var newEditorValues = this.state.editorValues;
+    newEditorValues.%(name)s = value;
+    this.setState({editorValues: newEditorValues});
+  },""" % propertyType.attrib
+        print """
+  render: function() {
+    return (
+      <div className="%(name)sForm">
+        <Button label='Add %(label)s' onClick={this.handleToggle} />
+        <Dialog
+          actions={this.state.actions}
+          active={this.state.editorActive}
+          onEscKeyDown={this.handleToggle}
+          onOverlayClick={this.handleToggle}
+          title='Add %(label)s'
+        >
+          <form>
+              """ %  dict(name=objType.get('name'),label=NodeWrapper(root,objType).getLabelList())
+
+        for propertyType in objType.findall('propertyType'):
+            propertyWrapper = PropertyWrapper(root,propertyType)
+            propertyWrapper.emitFormElement()
+        print """
+          </form>
+        </Dialog>
+      </div>
+    );
+  }
+});
+        """
 
     print """
 ReactDOM.render(
